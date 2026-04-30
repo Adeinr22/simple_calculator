41cdba7a4ef98d934e9a5d61e30459112b5f8938
Adeinr22 <andrei05.jules22@gmail.com>
2026-04-30 13:05:12 +0800

    setted up code plans and structure

---
+++ simple_calculator.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+class App():
+    def __init__(self):
+        pass
+
+class Calculator(App):
+    def __init__(self):
+        pass
+
+    def addition(self):
+        pass
+
+    def substraction(self):
+        pass
+
+    def multiplication(self):
+        pass
+
+    def division(self):
+        pass
+
+    def clear(self):
+        pass
+
+    def result(self):
+        pass
+
